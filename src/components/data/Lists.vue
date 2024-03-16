55a15fbd2ab7f80549888d234ffbf23e08be6fa4
Shem <macapobresshem@gmail.com>
2024-03-16 22:27:11 +0800

    Added list component

---
+++ src/components/data/Lists.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+defineProps<{
+    items: string[],
+    inline?: boolean
+}>();
+
+</script>
+
+<template>
+    <ul :class="{
+        'list': true,
+        'list--inline': inline
+    }">
+        <li v-for="item in items" :key="item" class="list__item">{{ item }}</li>
+    </ul>
+</template>
