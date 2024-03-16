94035b93d47411833bdc0129609d8604d6fe281b
Shem <macapobresshem@gmail.com>
2024-03-16 22:26:14 +0800

    Added collapsible and drawer components

---
+++ src/components/containers/Collapsible.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isToggled = ref(false);
+
+function toggle() {
+    isToggled.value = !isToggled.value;
+}
+
+</script>
+<template>
+    <div :class="{
+        'collapsible': true,
+        'collapsible--expanded': isToggled
+    }">
+        <header class="collapsible__header" @click="toggle">
+            <p class="collapsible__title">
+                <slot />
+            </p>
+            <slot name="icon" />
+        </header>
+        <div class="collapsible__content">
+            <slot name="content" />
+        </div>
+    </div>
+</template>
---
+++ src/components/containers/Drawer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+defineProps<{
+    isDrawerOpen: boolean
+}>();
+
+const emit = defineEmits(['update:isDrawerOpen'])
+
+</script>
+
+<template>
+    <div :class="{
+        'drawer': true,
+        'drawer--open': isDrawerOpen
+    }">
+        <div class="drawer__content">
+            <slot />
+        </div>
+        <IconCloseMark class="drawer__close" @click="$emit('update:isDrawerOpen', !isDrawerOpen)" />
+    </div>
+</template>
