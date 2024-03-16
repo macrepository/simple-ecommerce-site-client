42fca8568abddd86643db3183ce68fec67403d1d
Shem <macapobresshem@gmail.com>
2024-03-16 22:25:14 +0800

    make components autoimport

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,29 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    // your plugin installation
+    Components({
+      // relative paths to the directory to search for components.
+      dirs: ['src/components'],
+
+      // valid file extensions for components.
+      extensions: ['vue'],
+
+      // search for subdirectories
+      deep: true,
+
+      // generate `components.d.ts` global declarations,
+      // also accepts a path for custom filename
+      // default: `true` if package typescript is installed
+      dts: true
+    })
   ],
   resolve: {
     alias: {
