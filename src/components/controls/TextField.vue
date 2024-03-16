706687a9e22de0534aeced8faa415150d31f8df9
Shem <macapobresshem@gmail.com>
2024-03-16 22:26:42 +0800

    Added textField compoents

---
+++ src/components/controls/TextField.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    noLabel?: boolean
+    inline?: boolean
+    label?: string
+    id: string
+    placeholder?: string
+    modelValue: any
+}>()
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <div :class="{
+        'input-group': true,
+        'input-group--nolabel': noLabel,
+        'input-group--inline': inline
+    }">
+        <label v-if="!noLabel" :for="id">{{ label }}</label>
+        <input :id="id" :value="modelValue"
+            @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" type="text"
+            :placeholder="placeholder" />
+    </div>
+</template>
