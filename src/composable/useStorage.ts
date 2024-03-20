c528926defc4d81a35343bbeb0d81ce6d88dc9dd
Shem <macapobresshem@gmail.com>
2024-03-20 21:13:13 +0800

    Refactored storage and added session storage

--- src/composable/useStorage.ts
+++ src/composable/useStorage.ts
@@ -1,22 +1,59 @@
 import { watch, ref } from 'vue'
 import type { Ref } from 'vue'
 
-export function useStorage<T>(key: string, defaultValue: T | null = null): Ref<T> {
-  const item = localStorage.getItem(key)
-  const storedValue: T | null = item ? JSON.parse(item) : defaultValue
-  const value: Ref = ref<T | null>(storedValue)
+export function useLocalStorage() {
+  function createStorage<T>(key: string, data: T | null = null): Ref<T> {
+    const item = localStorage.getItem(key)
+    const storedValue: T | null = item ? JSON.parse(item) : data
+    const value: Ref = ref<T | null>(storedValue)
 
-  watch(
-    value,
-    (newValue) => {
-      if (newValue === null || newValue === '') {
-        localStorage.removeItem(key)
-      } else {
-        localStorage.setItem(key, JSON.stringify(newValue))
+    watch(
+      value,
+      (newValue) => {
+        if (newValue === null || newValue === '') {
+          removeLocalStorage(key)
+        } else {
+          localStorage.setItem(key, JSON.stringify(newValue))
+        }
+      },
+      { deep: true }
+    )
+
+    return value
+  }
+
+  function removeLocalStorage(key: string): void {
+    localStorage.removeItem(key)
+  }
+
+  return { createStorage, removeLocalStorage }
+}
+
+export function useSessionStorage() {
+  function setSession<T>(key: string, data: T | null = null): void {
+    if (data == null || data == '') {
+      removeSession(key)
+    } else {
+      sessionStorage.setItem(key, JSON.stringify(data))
+    }
+  }
+
+  function getSession<T>(key: string): T | null {
+    const session = sessionStorage.getItem(key)
+    if (session) {
+      try {
+        return JSON.parse(session)
+      } catch (error) {
+        console.error('Error parsing session storage item', error)
       }
-    },
-    { deep: true }
-  )
+    }
 
-  return value
+    return null
+  }
+
+  function removeSession(key: string): void {
+    sessionStorage.removeItem(key)
+  }
+
+  return { setSession, getSession, removeSession }
 }
