29a9db2757d4d41f9086b1c6ba7be99751ec7d3f
Shem <macapobresshem@gmail.com>
2024-03-21 21:38:12 +0800

    Make the product card to accept props data and adjusted the layout for absolute positioning of the button

--- src/components/containers/ProductCard.vue
+++ src/components/containers/ProductCard.vue
@@ -1,17 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+    link: string
+    image: string
+    alt: string
+    price: string
+    isSoldOut?: boolean
+    productId: number
+}>()
+</script>
+
 <template>
     <div class="product-card">
-        <RouterLink to="/shoes">
+        <RouterLink :to="link">
             <figure class="product-card__figure">
-                <img class="product-card__photo" src="/shoes.jpg" alt="running shoes">
-                <figcaption class="product-card__name">running shoes</figcaption>
+                <img class="product-card__photo" :src="image" :alt="alt">
+                <figcaption class="product-card__name"><slot /></figcaption>
             </figure>
         </RouterLink>
         <div class="product-card__information">
-            <span class="product-card__price">P 2000</span>
-            <Badge primary vertical class="product-card__sold">1 sold</Badge>
+            <span class="product-card__price">{{ price }}</span>
+            <Badge primary vertical class="product-card__sold" v-if="isSoldOut">sold out</Badge>
         </div>
         <div class="product-card__actions">
-            <Button primary>Add to cart</Button>
+            <Button primary v-if="!isSoldOut">Add to cart</Button>
         </div>
     </div>
 </template>
--- src/views/product/AllProductView.vue
+++ src/views/product/AllProductView.vue
@@ -4,17 +4,70 @@
 <template>
     <div class="container">
         <ProductList>
-            <ProductCard></ProductCard>
-            <ProductCard></ProductCard>
-            <ProductCard></ProductCard>
-            <ProductCard></ProductCard>
-            <ProductCard></ProductCard>
-            <ProductCard></ProductCard>
-            <ProductCard></ProductCard>
-            <ProductCard></ProductCard>
-            <ProductCard></ProductCard>
-            <ProductCard></ProductCard>
-            <ProductCard></ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1" isSoldOut>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1" >
+                Lorem ipsum dolor
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1" isSoldOut>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1" isSoldOut>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1" isSoldOut>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1" isSoldOut>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1" isSoldOut>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1" isSoldOut>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+            <ProductCard link="/shoes" image="/shoes.jpg" alt="running shoes" price="P 200" productId="1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, labore dolores dolorum magnam
+                assumenda deserunt.
+            </ProductCard>
+
         </ProductList>
     </div>
 </template>
